b1b84c25b00f3fa0efe9834d6d2e1dcc147d9836
Atsushi Mori <ki4070ma@gmail.com>
2019-03-24 01:49:08 +0900

    Added exception in isValid

--- app/src/main/java/amori/rssapp/InputChecker.kt
+++ app/src/main/java/amori/rssapp/InputChecker.kt
@@ -3,7 +3,7 @@ package amori.rssapp
 
 class InputChecker {
     fun isValid(text: String?): Boolean {
-        if (text == null) return false
+        if (text == null) throw IllegalArgumentException("Cannot be null")
         return text.length >= 3 && text.matches(Regex("[a-zA-Z0-9]+"))
     }
 }
--- app/src/test/java/amori/rssapp/InputCheckerTest.kt
+++ app/src/test/java/amori/rssapp/InputCheckerTest.kt
@@ -7,12 +7,18 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.lang.IllegalArgumentException
 
+@RunWith(JUnit4::class)
 class InputCheckerTest {
+
+    lateinit var target: InputChecker
 
     @Before
     fun setUp() {
-
+        target = InputChecker()
     }
 
     @After
@@ -22,26 +28,37 @@ class InputCheckerTest {
 
     @Test
     fun isValid_given3_returnTrue() {
-        val target = InputChecker()
         assertThat(target.isValid("foo"), `is`(true))
     }
 
     @Test
+    fun isValid_givenAlphabetic_returnsTrue() {
+        assertThat(target.isValid("abc"), `is`(true))
+    }
+
+    @Test
+    fun isValid_givenNumeric_returnsTrue() {
+        assertThat(target.isValid("123"), `is`(true))
+    }
+
+    @Test
     fun isValid_givenAlphaNumeric_returnsTrue() {
-        val target = InputChecker()
         assertThat(target.isValid("abc123"), `is`(true))
     }
 
     @Test
     fun isValid_givenLessThan3_returnsFalse() {
-        val target = InputChecker()
         assertThat(target.isValid("ab"), `is`(false))
     }
 
     @Test
     fun isVaid_givenNonAlphabetNumeric_returnsFalse() {
-        val target = InputChecker()
         assertThat(target.isValid("abc@"), `is`(false))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun isValid_givenNull_throwsIllegalArgumentException() {
+        target.isValid(null)
     }
 
     @Test
