dc3aa9d28e073b3e74a4008723069c3c0e18ae58
Atsushi Mori <ki4070ma@gmail.com>
2019-03-24 01:59:05 +0900

    Replaced hamcrest with AssertionJ

--- app/src/test/java/amori/rssapp/InputCheckerTest.kt
+++ app/src/test/java/amori/rssapp/InputCheckerTest.kt
@@ -1,15 +1,12 @@
 package amori.rssapp
 
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.hasItem
-import org.hamcrest.Matchers.greaterThan
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import java.lang.IllegalArgumentException
+import org.assertj.core.api.Assertions.*
 
 @RunWith(JUnit4::class)
 class InputCheckerTest {
@@ -28,43 +25,36 @@ class InputCheckerTest {
 
     @Test
     fun isValid_given3_returnTrue() {
-        assertThat(target.isValid("foo"), `is`(true))
+        assertThat(target.isValid("foo")).isTrue()
     }
 
     @Test
     fun isValid_givenAlphabetic_returnsTrue() {
-        assertThat(target.isValid("abc"), `is`(true))
+        assertThat(target.isValid("abc")).isTrue()
     }
 
     @Test
     fun isValid_givenNumeric_returnsTrue() {
-        assertThat(target.isValid("123"), `is`(true))
+        assertThat(target.isValid("123")).isTrue()
     }
 
     @Test
     fun isValid_givenAlphaNumeric_returnsTrue() {
-        assertThat(target.isValid("abc123"), `is`(true))
+        assertThat(target.isValid("abc123")).isTrue()
     }
 
     @Test
     fun isValid_givenLessThan3_returnsFalse() {
-        assertThat(target.isValid("ab"), `is`(false))
+        assertThat(target.isValid("ab")).isFalse()
     }
 
     @Test
     fun isVaid_givenNonAlphabetNumeric_returnsFalse() {
-        assertThat(target.isValid("abc@"), `is`(false))
+        assertThat(target.isValid("abc@")).isFalse()
     }
 
     @Test(expected = IllegalArgumentException::class)
     fun isValid_givenNull_throwsIllegalArgumentException() {
         target.isValid(null)
-    }
-
-    @Test
-    fun testMachers() {
-        assertThat(1 + 1, `is`(2))
-        assertThat(100, greaterThan(50))
-        assertThat(listOf("for", "bar", "baz"), hasItem("bar"))
     }
 }
